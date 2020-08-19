75abb8795b2e2b79db9ed3509241f6feba46356a
hanseok <gkstjrajt@gmail.com>
2020-08-19 15:11:53 +0900

    request_obj complete

---
+++ WebContent/researchForm.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>설문 조사</title>
+</head>
+<body>
+<h2>설문 조사</h2>
+<form action="ResearchServlet" method="post">
+	<table>
+		<tr>
+			<td> 이름 : </td>
+			<td>
+				<input type = "text" name = "name" size = "20">
+			</td>
+		</tr>
+		<tr>
+			<td> 성별 : </td>
+			<td>
+				<input type="radio" name = "gender" value = "Male" checked="checked"> 남자
+				<input type="radio" name = "gender" value = "Female"> 여자
+			</td>
+		</tr>
+		<tr>
+			<td> 좋아하는 계절 : </td>
+			<td>
+				<input type="checkbox" name = "season" value = "봄"> 봄
+				<input type="checkbox" name = "season" value = "여름" checked="checked"> 여름
+				<input type="checkbox" name = "season" value = "가을"> 가을
+				<input type="checkbox" name = "season" value = "겨울"> 겨울
+			</td>
+		</tr>
+		<tr align = "center">
+			<td><input type="submit" value="전송"> </td>
+			<td><input type="reset" value="취소"> </td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
---
+++ src/web_study_03/ResearchServlet.java
@@ -0,0 +1,45 @@
+package web_study_03;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ResearchServlet")
+public class ResearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String[] seasons = request.getParameterValues("season");
+		if ( seasons == null ) {
+			seasons = new String[] {"선택한 항목이 없음"};
+		}
+		String arrSeason = Arrays.stream(seasons).collect(Collectors.joining(", "));
+		
+		request.setAttribute("name", name);
+		request.setAttribute("gender", gender);
+		request.setAttribute("season", arrSeason);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("researchResult.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
